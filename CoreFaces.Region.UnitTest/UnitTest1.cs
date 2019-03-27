075786bc5f22dc876e5ce1e2b0da8ed833e84378
huseyin-altunbas <huseyin.altunbas@tutkal.com.tr>
2019-03-27 11:06:31 +0300

    Projects Init

---
+++ CoreFaces.Region.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreFaces.Region.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
